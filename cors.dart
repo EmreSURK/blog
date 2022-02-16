62d5857bdd88637814ef7a344ae38934cc4187e9
Emre ŞURK <emre.surk@gmail.com>
2022-02-16 20:01:34 +0300

    Update cors.dart

--- cors.dart
+++ cors.dart
@@ -6,3 +6,15 @@ return Response.ok(
   'Access-Control-Allow-Headers': 'Origin, Content-Type',
   },
 );
+
+
+static Response createSuccessResponse(Map? data) {
+    return Response.ok(
+      jsonEncode(ApiResponseData(status: "ok", message: "", data: data).toJson()),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type',
+      },
+    );
+  }
