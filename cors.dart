abce7d3e58ec74b60d4d6be87ee09702b29b2751
Emre ŞURK <emre.surk@gmail.com>
2022-02-16 18:56:41 +0300

    Create cors.dart

---
+++ cors.dart
@@ -0,0 +1,8 @@
+return Response.ok(
+  "", // your data.
+  headers: {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Origin, Content-Type',
+  },
+);
